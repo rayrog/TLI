d4125bf387c9992a17232a27ea4a96fb7d71123f
Rayan ROGAI <rayan.rogai@hotmail.fr>
2017-03-06 11:00:17 +0100

    start using templating

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('login.php');
 
 session_start(); // start or resume session
 if(isset($_SESSION['isConnect'])){
-  echo file_get_contents ('pages/index');
+  echo file_get_contents ('fragments/index');
 } else {
   login();
 }
