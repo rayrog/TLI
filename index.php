796c5e496bdcb33c51f3f5e8bddc3cfb45bd33ae
Rayan ROGAI <rayan.rogai@hotmail.fr>
2017-03-07 17:32:06 +0100

    add pathology

--- index.php
+++ index.php
@@ -12,21 +12,24 @@ $twig->display("menu.html");
 if(isset($_GET['p'])){
 switch ($_GET['p']){
   case "help":
-    $page= "help.html";
+    $twig->display("help.html");
     break;
   case "mentions":
-    $page= "mentions.html";
+    $twig->display("mentions.html");
+    break;
+  case "symptome":
+    $twig->display("pathology.html");
     break;
   default:
-    $page= "index.html";
+    $twig->display("login.html");
+    $twig->display("index.html");
     break;
-}
-  $twig->display($page);
-} else { //Si on a pas d'arguments dans le GET
+
+} 
+}else { //Si on a pas d'arguments dans le GET
   $twig->display("login.html");
   $twig->display("index.html");
 }
 
 $twig->display("foot_page.html");
 ?>
-
